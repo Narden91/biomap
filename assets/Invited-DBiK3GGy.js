a56558883901d5f4eeb2c22da82da6aec14b62a8
Emanuele <eminik2006@gmail.com>
2026-02-06 22:09:34 +0100

    Updates

--- assets/Invited-DBiK3GGy.js
+++
@@ -1,6 +0,0 @@
-import{c as t,j as e}from"./index-BjVw6SuI.js";import{P as s}from"./PageHeader-Dew8fKH_.js";/**
- * @license lucide-react v0.563.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const a=[["path",{d:"M12 19v3",key:"npa21l"}],["path",{d:"M19 10v2a7 7 0 0 1-14 0v-2",key:"1vc78b"}],["rect",{x:"9",y:"2",width:"6",height:"13",rx:"3",key:"s6n7sd"}]],i=t("mic",a),r=()=>e.jsxs("div",{className:"pb-20",children:[e.jsx(s,{title:"Invited Speakers",subtitle:"Keynote speakers and invited talks.",icon:i}),e.jsx("div",{className:"max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-12 text-center",children:e.jsx("div",{className:"bg-white rounded-xl p-12 border border-sage-100 border-dashed",children:e.jsx("p",{className:"text-sage-500 text-lg mb-8",children:"Detailed information about invited speakers will be announced soon."})})})]});export{r as default};
